fc0258af9c004ce27cb48b1d5558829f39de2cde
Peng Fei Ye <pengfeiye1994@gmail.com>
2017-09-21 15:42:56 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "Queen";
+var cardFour = "King";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
