c4272c9d638af4fe9c97d74bade881ed69ce6d1b
Peng Fei Ye <pengfeiye1994@gmail.com>
2017-09-23 23:06:22 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-var cards = ["Queen","Queen","King","King"];
+var cards = [
+  {
+    rank:"queen",
+    suit:"hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank:"queen",
+    suit:"diamond",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank:"king",
+    suit:"hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank:"king",
+    suit:"diamond",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+alert("You found a match!");
 } else {
-console.log("Sorry, try again.");
+alert("Sorry, try again.");
 };
 };
 
 var flipCards = function(cardId){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
 
 if (cardsInPlay.length === 2) {
   checkForMatch();}
